b031756d137036b7eb55b42697c97f0401f3e81a
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-27 02:08:34 +0300

    [core] input controlled by Vue

--- events-starting-code/app.js
+++ events-starting-code/app.js
@@ -1,10 +1,14 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0
+      counter: 0,
+      name: ''
     };
   },
   methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
     add(num) {
       this.counter = this.counter + num;
     },
