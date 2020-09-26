13d83b1a57b5bf4b7cfa60de555c7c9f1aa79342
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-27 02:11:41 +0300

    [core] additional parameter with a builtin one to an event listener

--- events-starting-code/app.js
+++ events-starting-code/app.js
@@ -6,8 +6,8 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    setName(event) {
-      this.name = event.target.value;
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
     },
     add(num) {
       this.counter = this.counter + num;
