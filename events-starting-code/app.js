db0fc966cd2418399f3e52b26dd621390add9ca3
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-27 01:42:10 +0300

    [core] moved the in-lined JS logic into Vue methods

--- events-starting-code/app.js
+++ events-starting-code/app.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 0
     };
   },
+  methods: {
+    add() {
+      this.counter = this.counter + 1;
+    },
+    reduce() {
+      this.counter = this.counter - 1;
+    }
+  }
 });
 
 app.mount('#events');
