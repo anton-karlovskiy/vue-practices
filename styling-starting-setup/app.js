48585549aa37d015a307205b0b6a49555819dece
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-27 20:18:53 +0300

    classes & computed properties example

--- styling-starting-setup/app.js
+++ styling-starting-setup/app.js
@@ -7,6 +7,11 @@ const app = Vue.createApp({
       boxCSelected: false
     };
   },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    }
+  },
   methods: {
     boxSelected(box) {
       if (box === 'A') {
