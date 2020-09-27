eb27dc3ad129e473889fdcf2d3a704df17a7db5f
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-27 20:10:13 +0300

    [core] dynamic and hardcoded CSS

--- styling-starting-setup/app.js
+++ styling-starting-setup/app.js
@@ -10,11 +10,11 @@ const app = Vue.createApp({
   methods: {
     boxSelected(box) {
       if (box === 'A') {
-        this.boxASelected = true;
+        this.boxASelected = !this.boxASelected;
       } else if (box === 'B') {
-        this.boxBSelected = true;
+        this.boxBSelected = !this.boxBSelected;
       } else if (box === 'C') {
-        this.boxCSelected = true;
+        this.boxCSelected = !this.boxCSelected;
       }
     }
   }
