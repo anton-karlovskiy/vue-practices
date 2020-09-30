4e316bd0955148e8ad2991630295831da3a1cc72
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-30 18:49:33 +0300

    [core] component example

--- cmp-intro/app.js
+++ cmp-intro/app.js
@@ -2,7 +2,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: 'manuel',
@@ -18,12 +17,36 @@ const app = Vue.createApp({
         }
       ]
     };
+  }
+});
+
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com'
+      }
+    };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     }
   }
-});
+})
 
 app.mount('#app');
