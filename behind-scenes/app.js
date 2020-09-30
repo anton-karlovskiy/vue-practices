23282e545153d3bd6e6c994d90fc0cebb288fd61
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-30 07:51:21 +0300

    [core] refs example

--- behind-scenes/app.js
+++ behind-scenes/app.js
@@ -11,7 +11,8 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
     },
   },
 });
