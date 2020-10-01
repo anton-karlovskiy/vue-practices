f4ed991ee4a55fa7a7475598b5de20667a41087d
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-10-01 11:31:56 +0300

    [core] vue cli starter project

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <header><h1>My Friends</h1></header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
