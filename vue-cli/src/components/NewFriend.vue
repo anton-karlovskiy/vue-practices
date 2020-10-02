0bdad4a09fcdc07e74c783c1e02c8fe0c0611839
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-10-02 11:07:18 +0300

    [chore] adjusted CSS

---
+++ vue-cli/src/components/NewFriend.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <form>
+    <div>
+      <label>Name</label>
+      <input type="text" />
+    </div>
+
+    <div>
+      <label>Phone</label>
+      <input type="tel" />
+    </div>
+
+    <div>
+      <label>E-Mail</label>
+      <input type="email" />
+    </div>
+
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
