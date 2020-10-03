7b342ce79e93393d7272a27983b0b395a30b6217
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-10-03 22:59:45 +0300

    [core] template example

--- cmp-adv/src/components/BaseCard.vue
+++ cmp-adv/src/components/BaseCard.vue
@@ -1,6 +1,9 @@
 
 <template>
   <div>
+    <header>
+      <slot name="header"></slot>
+    </header>
     <slot></slot>
   </div>
 </template>
@@ -12,6 +15,11 @@ export default {
 </script>
 
 <style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 div {
   margin: 2rem auto;
   max-width: 30rem;
