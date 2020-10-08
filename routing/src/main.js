5266af408474825f7f2a0f29faf5c6abd9482b8c
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-10-08 21:21:42 +0300

    [core] Passing Params as Props

--- routing/src/main.js
+++ routing/src/main.js
@@ -12,7 +12,7 @@ const router = createRouter({
   routes: [
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers },
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
   ],
   linkActiveClass: 'active'
 });
