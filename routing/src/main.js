7fe209dd4770623b06fb245a505dbc3a2a3d59c6
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-10-08 23:57:38 +0300

    [core] afterEach guard

--- routing/src/main.js
+++ routing/src/main.js
@@ -70,7 +70,13 @@ router.beforeEach(function(to, from, next) {
   next();
 });
 
-const app = createApp(App)
+router.afterEach(function(to, from) {
+  // sending analytics data
+  console.log('Global afterEach');
+  console.log(to, from);
+});
+
+const app = createApp(App);
 
 app.use(router);
 
